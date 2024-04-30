c672ef6a43bb62e67599b4ab97d93ec8fda746f5
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-04-30 17:51:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -203,13 +203,14 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 <b> Size ⚙️: {file_size}</b>""" 
 
     IMDB_TEMPLATE_TXT = """
-<b>Query: {qurey}
+<b>𝗥𝗲𝘀𝘂𝗹𝘁  ➜ {qurey}
 
 IMDb Data:
 
 <b>🏷 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
+🌟 Rating: <a href={url}/ratings>{rating}</a> / 10
 </b>"""
     
     ALL_FILTERS = """
