f0c7e98d228afd2f4d7df9e5fda71ad3d1abc19c
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-11-15 13:36:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,9 +48,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://shubhajitdas065:shubhajitdas065@cluster0.sizmvm1.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://wuohrgrawxne:UcK9rfimgnBQfWZU@cluster0.ypzz8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Silicon')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
